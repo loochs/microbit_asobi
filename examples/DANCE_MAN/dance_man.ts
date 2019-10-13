78edc65bcb92cdb6cf9fb096e19c16df5e518489
kichinosukey <kichinosuke.fukuhara@gmail.com>
2019-10-13 22:24:27 +0900

    [update] dance man
    - msyk's idea was reflected

--- examples/DANCE_MAN/dance_man.ts
+++ examples/DANCE_MAN/dance_man.ts
@@ -23,6 +23,11 @@ class Arm {
             led.plot(3, this.chest_point)
             led.plot(4, this.chest_point + 1)
         }
+
+        else if (which == "both") {
+            led.plot(3, this.chest_point)
+            led.plot(4, this.chest_point + 1)
+        }
     }
 
     handsUp(which: string) {
@@ -35,6 +40,11 @@ class Arm {
             led.plot(3, this.chest_point)
             led.plot(4, this.chest_point - 1)
         }
+
+        else if (which == "both") {
+            led.plot(3, this.chest_point)
+            led.plot(4, this.chest_point - 1)
+        }
     }
 }
 
@@ -44,12 +54,27 @@ class Body {
     chest_point: number
     leg: Leg
     arm: Arm
-    constructor(temp0: number, hip_point: number, chest_point: number) {
+    constructor(temp0: number, hip_point = 2, chest_point = 1) {
         this.temp0 = temp0
         this.hip_point = hip_point
         this.chest_point = chest_point
         this.leg = new Leg(this.temp0, this.hip_point)
         this.arm = new Arm(this.temp0, this.chest_point)
+    }
+
+    footUpLeft() {
+        this.leg.footUpLeft()
+        this.pause()
+    }
+
+    footUpRight() {
+        this.leg.footUpRight()
+        this.pause()
+    }
+
+    footUp() {
+        this.leg.footUp()
+        this.pause()
     }
 
     handsDownRight() {
@@ -63,23 +88,25 @@ class Body {
     }
 
     handsDown() {
-        this.arm.handsDown("right")
-        this.arm.handsDown("left")
+        this.arm.handsDown("both")
+        this.arm.handsDown("both")
         this.pause()
     }
 
     handsUpRight() {
         this.arm.handsUp("right")
-        this
+        this.pause()
     }
 
     handsUpLeft() {
         this.arm.handsUp("left")
+        this.pause()
     }
 
     handsUp() {
-        this.arm.handsUp("right")
-        this.arm.handsUp("left")
+        this.arm.handsUp("both")
+        this.arm.handsUp("both")
+        this.pause()
     }
 
     hip_change(x: number) {
@@ -87,17 +114,23 @@ class Body {
         this.leg.set_hip(this.hip_point)
     }
 
-    straight() {
+    straight(how: string) {
         this.arm.stay()
         led.plot(2, 0)
         led.plot(2, this.chest_point)
         this.hip_change(2)
         led.plot(this.hip_point, 2)
-        this.leg.stand()
+
+        if (how == "wide") {
+            this.leg.wideStand()
+        }
+        else if (how == "normal") {
+            this.leg.normalStand()
+        }
         this.pause()
     }
 
-    shake(which: string) {
+    shake(which: string, how: string) {
         this.arm.stay()
         led.plot(2, 0)
         led.plot(2, 1)
@@ -110,7 +143,14 @@ class Body {
             this.hip_change(1)
             led.plot(this.hip_point, 2)
         }
-        this.leg.stand()
+
+        if (how == "wide") {
+            this.leg.wideStand()
+        }
+
+        else if (how == "normal") {
+            this.leg.normalStand()
+        }
         this.pause()
     }
     pause() {
@@ -121,20 +161,46 @@ class Body {
 class Leg {
     temp0: number
     hip_point: number
-    constructor(temp0: number, hip_point: number) {
+    leg_point: number
+    constructor(temp0: number, hip_point: number, leg_point = 3) {
         this.temp0 = temp0
         this.hip_point = hip_point
+        this.leg_point = leg_point
+    }
+
+    footUpLeft() {
+        led.plot(this.hip_point - 1, this.leg_point)
+        led.unplot(this.hip_point - 1, this.leg_point + 1)
+        led.plot(this.hip_point - 2, this.leg_point)
+    }
+
+    footUpRight() {
+        led.plot(this.hip_point + 1, this.leg_point)
+        led.unplot(this.hip_point + 1, this.leg_point + 1)
+        led.plot(this.hip_point + 2, this.leg_point)
+    }
+
+    footUp() {
+        this.footUpLeft()
+        this.footUpRight()
+    }
+
+    normalStand() {
+        led.plot(this.hip_point - 1, this.leg_point)
+        led.plot(this.hip_point - 1, this.leg_point + 1)
+        led.plot(this.hip_point + 1, this.leg_point)
+        led.plot(this.hip_point + 1, this.leg_point + 1)
     }
 
     set_hip(hip_point: number) {
         this.hip_point = hip_point
     }
 
-    stand() {
-        led.plot(this.hip_point - 1, 3)
-        led.plot(this.hip_point - 1, 4)
-        led.plot(this.hip_point + 1, 3)
-        led.plot(this.hip_point + 1, 4)
+    wideStand() {
+        led.plot(this.hip_point - 1, this.leg_point)
+        led.plot(this.hip_point - 2, this.leg_point + 1)
+        led.plot(this.hip_point + 1, this.leg_point)
+        led.plot(this.hip_point + 2, this.leg_point + 1)
     }
 }
 
@@ -145,9 +211,33 @@ class Man {
         this.body = new Body(temp0, 2, 1);
     }
 
-    handDown() {
+    footUp(how: string) {
         basic.clearScreen()
-        this.body.straight()
+        this.body.straight(how)
+        this.pause()
+        this.body.footUp()
+        this.pause()
+    }
+
+    footUpLeft(how: string) {
+        basic.clearScreen()
+        this.body.straight(how)
+        this.pause()
+        this.body.footUpLeft()
+        this.pause()
+    }
+
+    footUpRight(how: string) {
+        basic.clearScreen()
+        this.body.straight(how)
+        this.pause()
+        this.body.footUpRight()
+        this.pause()
+    }
+
+    handsDown(how: string) {
+        basic.clearScreen()
+        this.body.straight(how)
         this.pause()
         this.body.handsDownLeft()
         this.pause()
@@ -155,9 +245,9 @@ class Man {
         this.pause()
     }
 
-    handsUp() {
+    handsUp(how: string) {
         basic.clearScreen()
-        this.body.straight()
+        this.body.straight(how)
         this.pause()
         this.body.handsUpLeft()
         this.pause()
@@ -165,18 +255,26 @@ class Man {
         this.pause()
     }
 
-    standStraight() {
+    handsUpRight(how: string) {
         basic.clearScreen()
-        this.body.straight()
+        this.body.straight(how)
+        this.pause()
+        this.body.handsUpLeft()
         this.pause()
     }
 
-    shakeHip() {
+    standStraight(how: string) {
         basic.clearScreen()
-        this.body.shake("right")
+        this.body.straight(how)
+        this.pause()
+    }
+
+    shakeHip(how: string) {
+        basic.clearScreen()
+        this.body.shake("right", how)
         this.pause()
         basic.clearScreen()
-        this.body.shake("left")
+        this.body.shake("left", how)
         this.pause()
     }
 
@@ -186,15 +284,33 @@ class Man {
 }
 
 const man = new Man(100)
+let how = "normal" // or "wide"
 
 input.onButtonPressed(Button.A, function () {
-    man.standStraight()
-    man.shakeHip()
-    man.standStraight()
+    man.standStraight(how)
+    for (let index = 0; index < 4; index++) {
+        man.shakeHip(how)
+        man.standStraight(how)
+
+    }
 })
 
 input.onButtonPressed(Button.B, function () {
-    man.standStraight()
-    man.handDown()
-    man.handsUp()
+    man.standStraight(how)
+    for (let index = 0; index < 4; index++) {
+        man.handsDown(how)
+        man.handsUp(how)
+        man.footUpLeft(how)
+        man.footUpRight(how)
+        man.footUp(how)
+    }
+})
+
+input.onButtonPressed(Button.AB, function () {
+    man.standStraight(how)
+    for (let index = 0; index < 4; index++) {
+        man.shakeHip(how)
+        man.handsDown(how)
+        man.handsUp(how)
+    }
 })
