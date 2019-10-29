be143e716a7082c71e9cfaf90db12475c44d4775
kichinosukey <kichinosuke.fukuhara@gmail.com>
2019-10-29 22:10:37 +0900

    [update] rhythm and melody

--- examples/ANNPAN_SONG/annpan_song.ts
+++ examples/ANNPAN_SONG/annpan_song.ts
@@ -1,48 +1,45 @@
 function そうだ () {
     for (let index = 0; index < 2; index++) {
-        music.playTone(523, music.beat(BeatFraction.Whole))
+        music.playTone(494, music.beat(BeatFraction.Whole))
     }
-    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
     music.rest(music.beat(BeatFraction.Whole))
     music.rest(music.beat(BeatFraction.Half))
 }
 input.onButtonPressed(Button.A, function () {
-    そうだ()
-    恐れないで()
-    みんなの()
-    為に()
-    そうだ()
     恐れないで()
     友達さ()
 })
 function 恐れないで () {
     for (let index = 0; index < 3; index++) {
-        music.playTone(349, music.beat(BeatFraction.Half))
+        music.playTone(330, music.beat(BeatFraction.Half))
     }
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
 }
 function みんなの () {
-    music.playTone(587, music.beat(BeatFraction.Whole))
-    music.playTone(587, music.beat(BeatFraction.Half))
-    music.playTone(659, music.beat(BeatFraction.Half))
-    music.playTone(523, music.beat(BeatFraction.Whole))
-    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
 }
 function 為に () {
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Whole))
+    music.playTone(370, music.beat(BeatFraction.Whole))
     music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
     music.rest(music.beat(BeatFraction.Whole))
 }
 function 友達さ () {
-    music.playTone(587, music.beat(BeatFraction.Whole))
-    music.playTone(587, music.beat(BeatFraction.Half))
-    music.playTone(659, music.beat(BeatFraction.Half))
-    music.playTone(523, music.beat(BeatFraction.Whole))
-    music.playTone(494, music.beat(BeatFraction.Whole))
-    music.playTone(523, music.beat(BeatFraction.Double))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(494, music.beat(BeatFraction.Quarter))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
 }
 basic.forever(function () {
 	
